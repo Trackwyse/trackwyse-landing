ed14de4680a479ec76d7829fe11512bb45b2988c
JS <49812749+JS00001@users.noreply.github.com>
2023-01-31 14:01:45 -0500

    analytics i think

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,24 +18,6 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-PK35HYRJRN"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-PK35HYRJRN', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Component {...pageProps} />
     </main>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,31 @@
  */
 
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-PK35HYRJRN"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PK35HYRJRN', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body className="bg-tan">
         <Main />
         <NextScript />
